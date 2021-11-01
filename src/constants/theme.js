e09a2a00834578e3487104875329df59fe9c5888
magdielSilva <Magdiel Silva>
2021-11-01 11:50:30 -0300

    Adicionar Logo

---
+++ src/constants/theme.js
@@ -0,0 +1,4 @@
+export const primaryColor = "#5CB64650"
+export const neutralColor = "#000000"
+export const textColor = "#00000025"
+export const borderColor = "#00000025"
---
+++ src/constants/urls.js
@@ -0,0 +1,2 @@
+
+export const BASE_URL = "https://us-central1-missao-newton.cloudfunctions.net/futureEatsA"
