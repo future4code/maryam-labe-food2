64d2723d5381582480386b4c459673919e36b20d
magdielSilva <Magdiel Silva>
2021-11-01 17:44:44 -0300

    Restaurante - início

--- src/pages/RestaurantPage/RestaurantPage.js
+++ src/pages/RestaurantPage/RestaurantPage.js
@@ -1,11 +1,42 @@
-import React from "react"
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { BASE_URL } from "../../constants/urls";
+import { ContainerRestaurant, TitleRestaurante, Container, BoxTitle, BoxImageBack, ImageBack } from "./style";
+
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik1nbUg3Vk5VMXVGYVBmN0NPTm5tIiwibmFtZSI6Ik1hZ2RpZWwgU2l2YSIsImVtYWlsIjoiYXJxLm1hZ2RpZWxzaWx2YUBnbWFpbC5jb20iLCJjcGYiOiI0MjIuODgwLjI4OC01OCIsImhhc0FkZHJlc3MiOnRydWUsImFkZHJlc3MiOiJSdWEgTWlndWVsIEJhcmJhciwgNDAwIC0gVmlsYSBHdXN0YXZvIENvcnJlaWEiLCJpYXQiOjE2MzU3OTI5MzF9.wcLFpU-xiJHSNbSdtGEQQBTm9kgivJVPAU1raX7jd0k";
+const url = `${BASE_URL}/restaurants/1`;
 
 const RestaurantPage = () => {
-    return (
-        <div>
-            RestaurantPage
-        </div>
-    )
-}
+  const [dataRestaurant, setDataRestaurant] = useState({});
 
-export default RestaurantPage
+  useEffect(() => {
+    axios
+      .get(url, {
+        headers: {
+          auth: token,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setDataRestaurant(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+  return (
+    <Container>
+      <ContainerRestaurant>
+          <BoxTitle>
+        <TitleRestaurante>Restaurante</TitleRestaurante>
+        <BoxImageBack>
+            <ImageBack src="../../assets/back.png" />
+        </BoxImageBack>
+        </BoxTitle>
+      </ContainerRestaurant>
+    </Container>
+  );
+};
+export default RestaurantPage;
---
+++ src/pages/RestaurantPage/style.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const ContainerRestaurant = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 360px;
+  height: 834px;
+  border: 1px solid black;
+`;
+export const ImageRestaurante = styled.img`
+  width: 328px;
+  height: 120px;
+`;
+export const ImageBack = styled.img`
+  width: 8.7px;
+  height: 15.3px;
+  background-color: var(--black);
+`;
+export const BoxTitle = styled.div`
+  width: 175px;
+  height: 44px;
+  margin: 20px;
+  border: 1px solid black;
+`;
+
+export const BoxImageBack = styled.div`
+  width: 23px;
+  height: 24px;
+  margin: 10px 54px 10px 15px;
+  border: 1px solid black;
+`;
+export const TitleRestaurante = styled.div`
+  padding: 13px 45.5px 12px;
+  font-family: "Roboto";
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  color: var(--black);
+  width: 84px;
+  height: 19px;
+`;
