1e8f3b9ba6b0c1717700737d24e3b6b92bb48891
mhsouza88 <88038506+mhsouza88@users.noreply.github.com>
2021-11-02 13:44:08 -0300

    Cadastro de endereço funcionando e loading adicionado aos buttons

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import useForm from "../../hooks/useForm"
 import {ScreenContainer, LogoImage, InputsContainer, SignUpButtonContainer} from "./style"
 import logo from "../../assets/logo-future-eats-invert.png"
@@ -7,23 +7,38 @@ import {goToHome, goToSingUp} from "../../routes/coordinator"
 import { useHistory } from "react-router"
 import axios from "axios"
 import {BASE_URL} from "../../constants/urls"
+import { withStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+const StyledButton = withStyles({
+    root: {
+      padding: '0.7rem',
+      marginTop: '0.7rem',
+      marginBottom: '1rem'
+    }
+  })(Button)
+
 const LoginPage = () => {
 
     const history = useHistory()
     const [form, onChange, clear] = useForm({email: "", password: ""})
+    const [isLoading, setIsLoading] = useState(false)
     const onSubmitForm = (event) => {
         event.preventDefault()
         login()
     }
 
     const login = () => {
+        setIsLoading(true)
         axios.post(`${BASE_URL}/login`, form)
         .then((res) => {
             localStorage.setItem("token", res.data.token)
             clear()
+            setIsLoading(false)
             goToHome(history)
         })
         .catch((error) => {
+            setIsLoading(false)
             alert("Erro no login")
         })
     }
@@ -59,14 +74,14 @@ const LoginPage = () => {
                      inputProps={{ pattern: "^.{6,}" }}
                    /> 
 
-                   <Button
+                   <StyledButton
                      type={"submit"}
                      fullWidth
                      variant={"contained"}
                      color={"primary"}
                    >
-                       Entrar
-                   </Button>
+                       { isLoading ? <CircularProgress color={"inherit"} size={24}/> : <>Entrar</> }
+                   </StyledButton>
                </form>
             </InputsContainer>
             <SignUpButtonContainer
--- src/services/putServices.js
+++ src/services/putServices.js
@@ -1,23 +1,26 @@
 import axios from 'axios'
 import { BASE_URL } from '../constants/urls'
-import { goToRestaurant } from '../routes/coordinator'
+import { goToProfile } from '../routes/coordinator'
 
-export const putAddAdress = ( body,history,cleanFields) => {
+export const putAddAdress = ( body, cleanFields, history, setIsLoading) => {
     const config = {
         method: 'put',
         url: BASE_URL + "/address",
         headers: { 'auth': localStorage.getItem('token') },
         data: body,
     }
-
+    setIsLoading(true)
     axios(config)
         .then((res) => {
             localStorage.setItem('token',res.data.token)
-            goToRestaurant(history)
+            window.alert("Seu enderço foi salvo!")
             cleanFields()
+            setIsLoading(false)
+            goToProfile(history)
         })
         .catch((err) => {
-            window.alert(err.response.data.message)
+            setIsLoading(false)
+            window.alert(err.data.message)
         })
 
 }
