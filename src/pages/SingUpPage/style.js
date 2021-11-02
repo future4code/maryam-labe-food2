852e9e10b408098b60fa841d45673e63be819b71
mhsouza88 <88038506+mhsouza88@users.noreply.github.com>
2021-11-01 21:35:26 -0300

    Cadastro funcionando

---
+++ src/pages/SingUpPage/style.js
@@ -0,0 +1,49 @@
+import styled from "styled-components"
+
+
+export const ScreenContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center; 
+  width: 100vw; 
+`
+
+export const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  max-width: 450px;
+  align-items: center;
+  margin-bottom: 20px;
+`
+
+export const LoginFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  max-width: 450px;
+  align-items: center;
+  margin-bottom: 20px;
+`
+
+export const ArrowBackContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: -40rem;
+  margin-left: -25rem;
+
+  @media screen and (min-device-width: 200px) and (max-device-width: 667px) {    
+    margin-top: -40rem;
+    margin-left: -16rem;
+  }
+`
+
+export const LogoImage = styled.img`
+  width: 104px;
+  height: 58px;
+  margin: 90px 128px 16px;
+  object-fit: contain;
+`
+
+
+
