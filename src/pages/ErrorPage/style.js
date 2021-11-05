a4fb65cf3e79cd6a0823a3603a62531b7d615edc
ArgLD <argleydson@gmail.com>
2021-11-05 09:10:25 -0300

    fiz a ErrorPage

---
+++ src/pages/ErrorPage/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const ErrorPageContainer = styled.div `
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px;
+  max-width: 350px;
+    
+    button { 
+        margin-top: 15vh;
+    }
+`
+
+export const ErrorImage = styled.img `
+  width: 64vw;
+  max-width: 450px;
+`
